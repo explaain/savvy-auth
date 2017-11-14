839983699e69055b4a7453c8a7170bd42c071bb7
Adam Snow <adamjohnsnow@icloud.com>
2017-11-14 10:35:21 +0000

    tweak testing

--- app/routes/authorisations.js
+++ app/routes/authorisations.js
@@ -1,32 +1,32 @@
-var express = require('express');
-var router = express.Router();
-var gdriveImporter = require('../controller/gdriveImport');
-var confluenceImporter = require('../controller/confluenceImport');
+var express = require('express')
+var router = express.Router()
+var gdriveImporter = require('../controller/gdriveImport')
+var confluenceImporter = require('../controller/confluenceImport')
 
-router.get('/update', function(req, res){
-  gdriveImporter.updateSourceFiles();
-  confluenceImporter.updateSourceFiles();
-  res.redirect('/');
-});
-
-router.get('/add-gdrive', function(req, res){
-  res.redirect(gdriveImporter.getCode());
-});
-
-router.get('/gdrive-token', function(req, res){
-  gdriveImporter.exchangeToken(req.query.code);
-  res.redirect('/');
+router.get('/update', function(req, res) {
+  gdriveImporter.updateSourceFiles()
+  confluenceImporter.updateSourceFiles()
+  res.redirect('/')
 })
 
-router.get('/add-confluence', function(req, res){
-  res.render('add-confluence.html', { title: 'Express' });
+router.get('/add-gdrive', function(req, res) {
+  res.redirect(gdriveImporter.getCode())
 })
 
-router.get('/save-confluence', function(req, res){
+router.get('/gdrive-token', function(req, res) {
+  gdriveImporter.exchangeToken(req.query.code)
+  res.redirect('/')
+})
+
+router.get('/add-confluence', function(req, res) {
+  res.render('add-confluence.html', { title: 'Express' })
+})
+
+router.get('/save-confluence', function(req, res) {
   console.log(req.query)
-  confluenceImporter.saveConfluenceDetails(req.query);
-  confluenceImporter.updateSourceFiles();
-  res.redirect('/');
+  confluenceImporter.saveConfluenceDetails(req.query)
+  confluenceImporter.updateSourceFiles()
+  res.redirect('/')
 })
 
-module.exports = router;
+module.exports = router
--- spec/gDriveImportSpec.js
+++ spec/gDriveImportSpec.js
@@ -4,8 +4,8 @@ var importer = require('../app/controller/gdriveImport.js')
 describe('Google Drive Importer', function() {
 
   it('get new code', function() {
-    var code = importer.getCode().substring(0, 42)
-    expect(code).toEqual('https://accounts.google.com/o/oauth2/auth?')
+    var url = importer.getCode().substring(0, 42)
+    expect(url).toEqual('https://accounts.google.com/o/oauth2/auth?')
   })
 
   it('Retrieve files', function(done) {
